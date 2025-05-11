42dc2adf2862da7d7983829ea8ddfdd6b26f8aca
liongex <isaclimaf2@gmail.com>
2025-05-11 18:25:55 -0300

    Adição de lógica da catraca

--- Tarefa_Catraca.c
+++ Tarefa_Catraca.c
@@ -32,11 +32,11 @@
 #define VRX 27 //Porta ADC de variação do X do joystick
 #define SW 22 //Pino do Botão do Joystick
 
-#define BOTTON_A 5 //Porta ADC de variação do Y do Joystick
-#define BOTTON_B 6 //Porta ADC de variação do X do joystick
-
 #define LED_COUNT 25
 #define LED_PIN 7
+
+// Define o tempo de debounce em milissegundos
+#define DEBOUNCE_TIME_MS 150
 
 const int ADC_CHANNEL_0 = 0; // Canal ADC para o eixo X do joystick
 const int ADC_CHANNEL_1 = 1; // Canal ADC para o eixo Y do joystick
@@ -47,12 +47,18 @@ PIO np_pio;
 uint sm;
 
 // Declaração do buffer de pixels que formam a matriz.
-npLED_t leds[LED_COUNT];
+ npLED_t leds[LED_COUNT];
 
 static uint16_t vrx_value, vry_value; 
 
+static volatile uint GR = 0, HO = 0, DI = 0, PT = 1;
+volatile uint32_t last_interrupt_time = 0;
+static int menu = 0;
+
 void setup_joystick(); // função para configurar adc
 void npInit(uint pin); //Inicializa a máquina PIO para controle da matriz de LEDs.
+static void gpio_irq_handler(uint gpio, uint32_t events);// função de callback da interrupção
+void catraca(); // Função de controle da catraca
 
 int main() {
 
@@ -62,16 +68,8 @@ int main() {
   setup_joystick();
   setup_rgb(LED_R,LED_G);
 
-  gpio_put(LED_G, true);
-  gpio_put(LED_R, true);
-  
   npInit(LED_PIN); // Inicialização da Maquina de Estado para a Matriz de Leds.
   npClear(leds, LED_COUNT); // Limpeza do buffer de leds.
-  npSetLED(LED_PIN0, 122, 0, 0, leds);
-  npSetLED(LED_PIN1, 122, 0, 0, leds);
-  npSetLED(LED_PIN2, 122, 0, 0, leds);
-  npSetLED(LED_PIN3, 122, 0, 0, leds);
-  npWrite(LED_COUNT,leds, np_pio,sm);
 
   uint countdown = 0; //verificar seleções para baixo do joystick
   uint countup = 3; //verificar seleções para cima do joystick
@@ -83,6 +81,8 @@ int main() {
   uint bar_y_pos;
 
   print_menu(pos_y); // Impressão inicial do menu
+
+  gpio_set_irq_enabled_with_callback(SW, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler); //Função de interrupção para controle do menu através do botão
 
   // Não faz mais nada. Loop infinito.
   while (true) {
@@ -99,20 +99,49 @@ int main() {
             pos_y+=12;
             countdown+=1;
             countup-=1;
+            if(menu <= 3){
+                menu += 1;
+            }
 
         }else if(bar_y_pos > 19 && countup <3){
             printf("valor de bar %d\n", bar_y_pos);
             pos_y-=12;
             countup+=1;
             countdown-=1;
+            if(menu >= 0){
+                menu -= 1;
+            }
         }
             //texto do Menu
             if(pos_y!=posy_ant){//verifica se houve mudança de posição no menu.
                 print_menu(pos_y);
             }
 
-        sleep_ms(100);//delay de atualização
         posy_ant=pos_y;//atualização posição anterior.
+
+        if(GR == 1){
+            npSetLED(LED_PIN0, 0, 80, 0, leds);    
+        }else{
+            npSetLED(LED_PIN0, 80, 0, 0, leds);   
+        }
+        if(HO == 1){
+            npSetLED(LED_PIN1, 0, 80, 0, leds);    
+        }else{
+            npSetLED(LED_PIN1, 80, 0, 0, leds);   
+        }
+        if(DI == 1){
+            npSetLED(LED_PIN2, 0, 80, 0, leds);    
+        }else{
+            npSetLED(LED_PIN2, 80, 0, 0, leds);   
+        }
+        if(PT == 0){
+            npSetLED(LED_PIN3, 0, 80, 0, leds);    
+        }else{
+            npSetLED(LED_PIN3, 80, 0, 0, leds);   
+        }
+        npWrite(LED_COUNT,leds,np_pio,sm);
+        catraca();
+        sleep_ms(100);//delay de atualização
 
     }
 }
@@ -156,3 +185,52 @@ void npInit(uint pin) {
       leds[i].B = 0;
     }
 }
+
+//Função da callback da interrupção
+
+static void gpio_irq_handler(uint gpio, uint32_t events){
+
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    switch (menu) {
+        case 0:
+            if ((current_time - last_interrupt_time) > DEBOUNCE_TIME_MS) {
+                GR = !GR;
+                last_interrupt_time = current_time;
+            }
+            break;
+        case 1:
+            if ((current_time - last_interrupt_time) > DEBOUNCE_TIME_MS) {
+                HO = !HO;
+                last_interrupt_time = current_time;
+            }
+            break;
+        case 2:
+            if ((current_time - last_interrupt_time) > DEBOUNCE_TIME_MS) {
+                DI = !DI;
+                last_interrupt_time = current_time;
+            }
+        break;
+        case 3:
+            if ((current_time - last_interrupt_time) > DEBOUNCE_TIME_MS) {
+                PT = !PT;
+                last_interrupt_time = current_time;
+            }
+        break;
+        default:
+            printf("Número inválido!\n");
+    }
+};
+
+// Função de controle da catraca
+void catraca(){
+    if(GR && HO && DI){
+        gpio_put(LED_G, true);
+        gpio_put(LED_R, false);
+    } else if(!PT){
+        gpio_put(LED_G, true);
+        gpio_put(LED_R, false);
+    } else {
+        gpio_put(LED_G, false);
+        gpio_put(LED_R, true);
+    };
+};
